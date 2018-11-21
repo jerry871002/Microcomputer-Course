3cd454594c903a42f937ca1f165a14468413cbd0
jerry <jerry871002@gmail.com>
2018-11-21 12:11:28 +0800

    Lab7腳位小更新

--- Lab07/lab7_1.asm
+++ Lab07/lab7_1.asm
@@ -4,33 +4,33 @@
 MAIN:
 	MOV DPTR, #NUMTABLE
 ROW1:
-	MOV P1, #7FH 		;掃描第一個row(s01~s04)
+	MOV P0, #7FH 		;掃描第一個row(s01~s04)
 	CALL DELAY
-	MOV A, P1
+	MOV A, P0
 	ANL A, #0FH 		;將較高的4-bit清為0
 	MOV R1, #0
 	CJNE A, #0FH, COL1	;確認s01~s04是否有被按下
 						;有則至掃描col的地方
 ROW2:					;沒有則往下掃第二個row(s05~s08)
-	MOV P1, #0BFH 		;掃描第二個row(s05~s08)
+	MOV P0, #0BFH 		;掃描第二個row(s05~s08)
 	CALL DELAY
-	MOV A, P1
+	MOV A, P0
 	ANL A, #0FH 		;將較高的4-bit清為0
 	MOV R1, #4
 	CJNE A, #0FH, COL1	;確認s05~s08是否有被按下
 						;有則至掃描col的地方
 ROW3:					;沒有則往下掃第三個row(s09~s12)
-	MOV P1, #0DFH 		;掃描第三個row(s09~s12)
+	MOV P0, #0DFH 		;掃描第三個row(s09~s12)
 	CALL DELAY
-	MOV A, P1
+	MOV A, P0
 	ANL A, #0FH 		;將較高的4-bit清為0
 	MOV R1, #8
 	CJNE A, #0FH, COL1	;確認s09~s12是否有被按下
 						;有則至掃描col的地方
 ROW4:					;沒有則往下掃第四個row(s13~s16)
-	MOV P1, #0EFH 		;掃描第四個row(s13~s16)
+	MOV P0, #0EFH 		;掃描第四個row(s13~s16)
 	CALL DELAY
-	MOV A, P1
+	MOV A, P0
 	ANL A, #0FH 		;將較高的4-bit清為0
 	MOV R1, #12
 	CJNE A, #0FH, COL1	;確認s13~s16是否有被按下
@@ -56,8 +56,8 @@ SHOW:
 	ADD A, R0
 	MOVC A, @A+DPTR
 OVER:
-	MOV P2, #0DH
-	MOV P3, A
+	MOV P2, #0DH 		;0DH=00001101B
+	MOV P1, A
 	JMP ROW1
 
 DELAY:
