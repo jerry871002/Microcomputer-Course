efaf378fdfa80ad108ac31cf820e1d5d382790ca
jerry <jerry871002@gmail.com>
2018-12-21 22:50:55 +0800

    Lab13程式重新命名

--- Lab13/lab13_1.asm
+++ Lab13/lab13_1.asm
@@ -1,45 +1,34 @@
-    ORG 0000H
-    JMP MAIN
-    ORG 0050H
+	ORG 0000H
+	JMP MAIN
+	ORG 0050H
 MAIN:
-    MOV DPTR, #NUMTABLE
+	MOV DPTR, #NUMTABLE
+	
+	MOV TMOD, #00100000B	;Timer 1, Mode 2
+	MOV TL1, #0E6H			;Baud rate = 2400
+	MOV TH1, #0E6H			;初始值 E6H = 230
+	ORL PCON, #80H			;SMOD = 1
+	SETB TR1				;Timer Run
+	CLR SM2
+	CLR SM0					;Serial port Mode 1
+	SETB SM1				;設為8位元UART
 
-    MOV TMOD, #00100000B	;Timer 1, Mode 2
-    MOV TL1, #0E6H			;Baud rate = 2400
-    MOV TH1, #0E6H			;初始值 E6H = 230
-    ;ORL PCON, #80H			;SMOD = 1
-    SETB TR1                ;Timer Run
-    CLR SM2
-    CLR SM0                 ;Serial port Mode 1
-    SETB SM1                ;設為8位元UART
+	SETB REN				;Receive Enable control bit
 
-    SETB REN				;Receive Enable control bit
-
-    CLR RI
-    JNB RI, $
 LOOP:
-    MOV A, SBUF
-    SUBB A, #30H            ;因為傳進去的值是ASCII，要減48
-    MOVC A, @A+DPTR
-    CLR RI
+	CLR RI
+	JNB RI, $
+	MOV A, SBUF
+	SUBB A, #30H			;因為傳進去的值是ASCII，要減48
+	MOVC A, @A+DPTR
+	MOV R0, A
 
 SHOW: 						;顯示到七段顯示器上
-    MOV P0, #0F7H
-    MOV P1, A
-    CALL DELAY
+   	MOV P0, #11111110B
+	MOV P1, R0
+	CALL DELAY
 
-    JB RI, LOOP
-
-    JMP SHOW
-
-DELAY:
-    MOV R6,#50
-DELAY1:
-    MOV R7,#50
-DELAY2:
-    DJNZ R7,DELAY2
-    DJNZ R6,DELAY1
-    RET
+	JMP LOOP
 
 NUMTABLE:
 	DB 0C0H ;0
@@ -52,5 +41,14 @@ NUMTABLE:
 	DB 0F8H ;7
 	DB 080H ;8
 	DB 090H ;9
+
+DELAY:
+	MOV R6, #50
+DELAY1:
+	MOV R7, #50
+DELAY2:
+	DJNZ R7, DELAY2
+	DJNZ R6, DELAY1
+	RET
 
 END
