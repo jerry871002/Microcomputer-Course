d5e0d392f658a2eb878450b71f56830c7177d58d
jerry <jerry871002@gmail.com>
2019-01-14 07:56:46 +0800

    主要功能完成+改檔名

--- 期末專題/final.asm
+++ 期末專題/final.asm
@@ -6,7 +6,13 @@
     JMP INT1INTERUPT
     ORG 0050H
 INITIAL:
+    MOV 31H, #0FEH
+    MOV A, 31H
+    MOV P1, A
     MOV R4, #255
+
+    CLR P3.2
+
     MOV DPTR, #NUMTABLE
     SETB IT1				;Falling edge trigger for interrupt 1 (TCON.2)
     SETB PX1				;Set INT1 interrupt priority (IP.2)
@@ -27,6 +33,8 @@ INITIAL:
     SETB TR1                ;Counter 1開始運作
 
 SHOW:
+
+
     MOV P0, #0FBH           ;百位
 
     MOV A, R2
@@ -54,7 +62,15 @@ SHOW:
     CALL DELAY
     MOV P2, #0FFH
 
+    ;MOV A, 31H
+    ;MOV P1, A
+
     DJNZ R4, SHOW
+
+    MOV A, 31H
+    RR A
+    MOV 31H, A
+    MOV P1, A
 
     SETB P3.4
     MOV R5, #1
@@ -68,6 +84,13 @@ SHOW:
 TIMER0INTERUPT:
     CPL P3.7
     CLR TF0
+    CJNE R3, #255, SOUND
+    JMP TIMERRETURN
+SOUND:
+    DJNZ R3, TIMERRETURN
+    CPL P3.2
+    MOV R3, 30H
+TIMERRETURN:
     RETI
 
 INT1INTERUPT:
@@ -81,10 +104,54 @@ INT1INTERUPT:
     MOV R1, A               ;十位數
     MOV R0, B               ;個位數
 
+    CJNE R2, #0, NEXT1
+
+    CJNE R1, #5, NEXT2
+NEXT2:
+    JC NEXT3
+    ;50 <= distance < 100, green
+    SETB P3.1               ;Red light
+    CLR P3.6                ;Green light
+    JMP SETSOUND
+NEXT3:
+    CJNE R1, #2, NEXT4
+NEXT4:
+    JC NEXT5
+    ;20 <= distance < 50, yellow(red + green)
+    CLR P3.1                ;Red light
+    CLR P3.6                ;Green light
+    JMP SETSOUND
+NEXT5:
+    ;distance < 20, red
+    CLR P3.1                ;Red light
+    SETB P3.6               ;Green light
+    JMP SETSOUND
+
+NEXT1:
+    SETB P3.1               ;Red light
+    SETB P3.6               ;Green light
+
+SETSOUND:
+    MOV A, R1               ;設定音調
+    MOV B, #10
+    MUL AB
+    ADD A, R0
+    MOV B, #2
+    DIV AB
+    ADD A, #17
+    MOV R3, A
+    MOV 30H, A
+
+    CJNE R2, #0, NEXT6
+    JMP INT1RETURN
+NEXT6:
+    MOV R3, #255
+    MOV 30H, #255
+
+INT1RETURN:
     CLR TF1                 ;Counter歸零
     MOV TH1, #0
     MOV TL1, #0
-
     RETI
 
 DELAY:
